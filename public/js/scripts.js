5fabeafca1491a180d4a972e710440efbd48705b
theFeez <zachfeez@knights.ucf.edu>
2017-11-07 18:02:38 -0500

    added spotify track searching

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,3 +8,17 @@ $('#clarifai').submit(function(e){
   });
   e.preventDefault();
 });
+
+$('#spotifyForm').submit(function(e){
+    var trackName = $('#trackName').val();
+    $.post('/trackSearch',{trackName:trackName},function(data){
+        console.log(data);
+        $('#trackList').html('');
+            var tracks = data.tracks.items;
+            for(var i in tracks){
+                console.log(tracks[i]);
+                $('#trackList').html($('#trackList').html()+'<li>'+tracks[i].name+' By: '+tracks[i].artists[0].name+'</li>');
+            }
+    })
+    e.preventDefault();
+})
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var request = require('request');
 const Clarifai = require('clarifai');
 const clarifaiApp = new Clarifai.App({
  apiKey: 'YOUR-API-KEY'
@@ -22,6 +23,39 @@ app.post('/getTags', function(req, res){
     }
   );
 });
+
+app.post('/trackSearch',function(req,res){
+    console.log(req.body);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        'Authorization': 'Basic '+(new Buffer('appId:appSecret').toString('base64'))
+      },
+      form: {
+        grant_type: 'client_credentials'
+      },
+      json: true
+    };
+
+    request.post(authOptions,function(err,response,body){
+        if(!err){
+            var token = body.access_token;
+            var searchOptions = {
+                url: 'https://api.spotify.com/v1/search?q='+req.body.trackName+'&type=track',
+                headers:{
+                    'Authorization': 'Bearer '+token
+                },
+                json: true
+            };
+
+            request.get(searchOptions,function(error,searchResponse,searchBody){
+                if(!error){
+                    res.send(searchBody);
+                }
+            })
+        }
+    })
+})
 
 app.listen(1337, function(){
   console.log('listening on port: 1337');
